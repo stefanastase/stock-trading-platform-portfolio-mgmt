17190ab5cbf1915a6a8f3a954c0aefde3144b399
stefan.nastase1512 <stefan.nastase1512@stud.acs.upb.ro>
2023-04-26 21:30:55 +0300

    Implement PUT operation for portofolio update

--- app.py
+++ app.py
@@ -58,6 +58,37 @@ def get_portfolio(client_ID):
             cursor.close()
             connection.close()
     
+@app.route("/portfolio/<client_ID>", methods=["PUT"])
+def update_portfolio(client_ID):
+    payload = request.get_json(force=True)
+
+    for key in payload.keys():
+        name = key
+        quantity = int(payload[key])
+        try:
+            connection = psycopg2.connect(host=host, dbname=db_name, user=db_user, password=db_pass)
+            cursor = connection.cursor()
+            query = f"\
+                UPDATE {client_ID}\
+                SET \"Quantity\" = {quantity}\
+                WHERE \"Name\" = \'{name}\'"
+            
+            cursor.execute(query)
+            connection.commit()
+
+            return Response(status=200)
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+            if connection is not None:
+                cursor.close()
+                connection.close()
+            return Response(status=400)
+
+        finally:
+            if connection is not None:
+                cursor.close()
+                connection.close()
+    return Response(status=400)
 
 if __name__ == "__main__":
     app.run(debug=False)
